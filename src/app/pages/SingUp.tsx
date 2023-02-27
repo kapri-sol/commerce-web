22bd635124e0ef990abbbc35f829deb36ac60567
kapri.kim <ksm6902@gmail.com>
2023-02-27 17:34:21 +0900

    change sign in

--- src/app/pages/SingUp.tsx
+++ src/app/pages/SingUp.tsx
@@ -2,43 +2,65 @@ import React from "react";
 
 const SignUp = () => {
     return (
-        <div className="pt-[100px] flex flex-col justify-center">
-            <div>Commerce</div>
-            <div className="w-full rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800">
-                <div>
-                    <h2 className="bg-blue-400 py-8 text-center">Sign Up</h2>
-                    <form className="py-8 my-8">
-                        <div className="bg-gray-300 mx-auto px-12 py-2">
-                            <div className="mb-4 my-2">
-                                <label className="mx-4 p-1 font-semibold">email</label>
-                                <input type={"email"} placeholder={"test@email.com"} className="shadow border p-1" />
+        <section className="bg-gray-50 dark:bg-gray-900">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Sign Up</h1>
+                        <form className="space-y-4 md:space-y-6">
+                            <div>
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                    email
+                                </label>
+                                <input
+                                    type="email"
+                                    placeholder="test@email.com"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    required={true}
+                                />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">name</label>
+                                <input
+                                    type="text"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">phone number</label>
+                                <input
+                                    type={"tel"}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="+82 10-1234-1234"
+                                />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">password</label>
+                                <input
+                                    type={"password"}
+                                    placeholder={"********"}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                />
                             </div>
                             <div className="mb-4 my-2">
-                                <label className="mx-4 p-1">name</label>
-                                <input type={"text"} className="shadow border p-1" />
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">check password</label>
+                                <input
+                                    type={"password"}
+                                    placeholder={"********"}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                />
                             </div>
-                            <div className="mb-4 my-2">
-                                <label className="mx-4 p-1">phone number</label>
-                                <input type={"tel"} className="shadow border p-1" />
-                            </div>
-                            <div className="mb-4 my-2">
-                                <label className="mx-4 p-1">password</label>
-                                <input type={"password"} placeholder={"********"} className="shadow border p-1" />
-                            </div>
-                            <div className="mb-4 my-2">
-                                <label className="mx-4 p-1">check password</label>
-                                <input type={"password"} placeholder={"********"} className="shadow border p-1" />
-                            </div>
-                            <div className="bg-red-300 mx-auto">
-                                <button type="submit" className="my-8">
-                                    Submit
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                            <button
+                                type="submit"
+                                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                            >
+                                Sign In
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
 
